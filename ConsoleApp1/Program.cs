30bf475c11bf2f86b81b165eab9f4e3c325ecf52
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-12-03 14:20:22 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = GetSize();
+            int[] omas = Input(size);
+            Output(omas);
+            Console.ReadKey();
+        }
+        static void Output(int[] crocodile)
+        {
+            for (int i = 0; i < crocodile.Length; i++)
+            {
+                Console.WriteLine($"crocodile[{i}] = {crocodile[i]}");
+            }
+        }
+        static int GetSize()
+        {
+            Console.Write("Введите размер массива = ");
+            int size;
+            while (! int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.Write("Ошибка");
+            }
+            return size;
+        }
+        static int[] Input(int size)
+        {
+            int[] crocodile = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"crocodile[{i}] = ");
+                crocodile[i] = int.Parse(Console.ReadLine());
+            }
+            return crocodile;
+        }
+    }
+}
